d7bd85c9258d9c5305a4deb4146485acc0b8a13a
Jessica Colliersmith <34167421+jcolliersmith@users.noreply.github.com>
2018-03-25 14:59:51 -0400

    cleanup

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var img = document.getElementById('Honeymoon');
+var modalImg = document.getElementById("img/honeymoon.jpg");
+var captionText = document.getElementById("Honeymoon in Spain");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
